7aee21685a9a9d16f7462ab99a603c41e3d29b36
IsaFoster <60882878+IsaFoster@users.noreply.github.com>
2021-09-30 16:33:51 +0200

    initial commit

--- src/Main/java/no/hvl/dat110/rest/counters/App.java
+++ src/Main/java/no/hvl/dat110/rest/counters/App.java
@@ -1,1 +1,49 @@
+package no.hvl.dat110.rest.counters;
 
+import com.google.gson.Gson;
+import java.util.HashMap;
+import java.util.Map;
+import static spark.Spark.*;
+
+public class App {
+
+	static Map<Long, Todo> todos = new HashMap<>();
+
+	public static void main(String[] args) {
+
+		if (args.length > 0) {
+			port(Integer.parseInt(args[0]));
+		} else {
+			port(8080);
+		}
+
+		after((req, res) -> res.type("application/json"));
+
+		get("/todo", (req, res) -> new Gson().toJson(todos.values()));
+
+		get("/todo/:id", (req, res) -> {
+			Long id = Long.parseLong(req.params("id"));
+			return todos.get(id).toJson();
+		});
+
+		post("/todo", (req, res) -> {
+			Todo todo = new Gson().fromJson(req.body(), Todo.class);
+			todos.put(todo.getId(), todo);
+			return todo.toJson();
+		});
+
+		delete("/todo/:id", (req, res) -> {
+			Long id = Long.parseLong(req.params("id"));
+			return todos.remove(id).toJson();
+		});
+
+		put("/todo/:id", (req, res) -> {
+			Long id = Long.parseLong(req.params("id"));
+			Todo newTodo = new Gson().fromJson(req.body(), Todo.class);
+			todos.replace(id, newTodo);
+			return todos.get(id).toJson();
+		});
+
+
+	}
+}
